695bece4e7fbbb6a2bc2705f79b2dcba04f98b52
Ayan209e <ayan209e@gmail.com>
2023-10-26 13:15:13 +0530

    Minor Bugs Resolved

--- src/Footer.js
+++ src/Footer.js
@@ -10,9 +10,9 @@ const Footer = () => {
                         <img src="./images/hotelamricafooter.png.png" alt=""/>
                     </div>
                     <div className="footer1_cont">
-                        <p><i className="fa fa-map-marker" aria-hidden="true"></i>123, Main Street,NY 456789</p>
-                        <p><i className="fa fa-mobile" aria-hidden="true"></i>+1234567890</p>
-                        <p><i className="fa fa-envelope" aria-hidden="true"></i>free@psdfreebles.com</p>
+                        <p><i className="fa fa-map-marker" aria-hidden="true"></i>  123, Main Street,NY 456789</p>
+                        <p><i className="fa fa-mobile" aria-hidden="true"></i>  +1234567890</p>
+                        <p><i className="fa fa-envelope" aria-hidden="true"></i>  free@psdfreebies.com</p>
                     </div>
                 </div>
                 <div className="footer2">
@@ -23,8 +23,13 @@ const Footer = () => {
                         <a href="#awards">PARTNERSHIP</a>
                 </div>
                 <div className="footer3">
-                    <p>we are in social network</p>
-                    <p><i className="fa fa-address-book" aria-hidden="true"></i></p>
+                    <p>We are in social network</p>
+                    <div className='socials'>
+                    <a href="https://www.instagram.com/" target='_blank'><i className="fa fa-instagram" aria-hidden="true" ></i></a>
+                    <a href="https://www.facebook.com" target='_blank'><i className="fa fa-facebook" aria-hidden="true"></i></a>
+                    <a href="https://www.twitter.com" target='_blank'><i className="fa fa-twitter" aria-hidden="true"></i></a>
+                    <a href="https://www.youtube.com" target='_blank'><i className="fa fa-youtube" aria-hidden="true"></i></a>
+                    </div>
                 </div>
             </div>
             <h2 className="footer-cont">Copyright 2017 PSDfreebles.com All Right Reserved</h2>
